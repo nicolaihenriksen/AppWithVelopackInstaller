5b7e15ace6b9296d1e9fefbbb51f70ace81d49e6
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-11-16 16:29:25 +0100

    Initial scaffold of app with installer

---
+++ AppWithVelopackInstaller/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AppWithVelopackInstaller;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public string AppVersion { get; } = typeof(MainWindowViewModel).Assembly.GetName().Version.ToString();
+
+    [RelayCommand]
+    private void CheckForUpdates()
+    {
+        // Check for updates
+    }
+}
